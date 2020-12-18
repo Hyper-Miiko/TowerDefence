3f6e348a1039c162052d2389f7002b28c93a71c3
Hyper-Miiko <geiishokunomiko@gmail.com>
2020-12-18 18:53:28 +0100

    :@

--- TowerDefence/src/fr/tm_nlm/tower_defence/control/Damageable.java
+++
@@ -1,7 +0,0 @@
-package fr.tm_nlm.tower_defence.control;
-
-public interface Damageable {
-	public abstract void damage(int value);
-	public abstract void heal(int value);
-	public abstract int getHealth();
-}
