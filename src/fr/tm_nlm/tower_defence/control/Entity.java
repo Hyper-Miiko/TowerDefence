143654c57f162b419baaa2d76fe2349166f78a69
Hyper-Miiko <geiishokunomiko@gmail.com>
2020-12-28 17:17:57 +0100

    override equal

--- src/fr/tm_nlm/tower_defence/control/Entity.java
+++ src/fr/tm_nlm/tower_defence/control/Entity.java
@@ -1,28 +1,44 @@
 package fr.tm_nlm.tower_defence.control;
 
-import java.util.ArrayList;
-
-import fr.tm_nlm.tower_defence.control.data.Image;
+import fr.tm_nlm.tower_defence.control.data.Appareances;
 import fr.tm_nlm.tower_defence.control.data.geometric.Vector;
 import fr.tm_nlm.tower_defence.control.data.geometric.Shape;
 
 public class Entity {
+	private static long nextId = 1;
 	
-	private Field field;
-	private Shape shape;
-	private ArrayList<Image> img;
+	protected Field field;
+	private Appareances appareances;
+	private final long id;
 
 	public Entity(Field field, Shape shape) {
 		this.field = field;
-		this.shape = shape;
-		img = new ArrayList<>();
+		appareances = new Appareances(shape);
+		id = nextId++;
 	}
 	
 	public Vector getPosition() {
-		return shape.getPosition();
+		return appareances.getShape().getPosition();
 	}
 
-	public Shape getShape() {
-		return shape;
+	public Appareances getAppareances() {
+		return appareances;
+	}
+	
+	@Override
+	public boolean equals(Object entity) {
+		if(!(entity instanceof Entity)) {
+			return false;
+		} else {
+			return this.id == ((Entity) entity).id;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return "Entité n°" + id +
+			   " aux positions " + appareances.getShape().getPosition() +
+			   " dans la carte " + field + 
+			   ".";
 	}
 }
---
+++ src/fr/tm_nlm/tower_defence/control/data/Appareances.java
@@ -0,0 +1,46 @@
+package fr.tm_nlm.tower_defence.control.data;
+
+import java.util.HashMap;
+
+import fr.tm_nlm.tower_defence.control.data.geometric.Shape;
+
+public class Appareances {
+	private HashMap<Posture, String> postures;
+	private Shape shape;
+	
+	public Appareances(Shape shape) {
+		this.shape = shape;
+		postures = new HashMap<>();
+	}
+	
+	public Shape getShape() {
+		return shape;
+	}
+
+	public enum Posture {
+		WALK_TL1,
+		WALK_TL2,
+		WALK_TL3,
+		WALK_T1,
+		WALK_T2,
+		WALK_T3,
+		WALK_TR1,
+		WALK_TR2,
+		WALK_TR3,
+		WALK_R1,
+		WALK_R2,
+		WALK_R3,
+		WALK_BR1,
+		WALK_BR2,
+		WALK_BR3,
+		WALK_B1,
+		WALK_B2,
+		WALK_B3,
+		WALK_BL1,
+		WALK_BL2,
+		WALK_BL3,
+		WALK_L1,
+		WALK_L2,
+		WALK_L3;
+	}
+}
