667abcf61e99788c875c1aa668bdbe8c17525e04
Hyper-Miiko <geiishokunomiko@gmail.com>
2020-12-28 17:33:51 +0100

    On peut récupérer un rectangle disrectement d'une shape

--- src/fr/tm_nlm/tower_defence/control/data/geometric/Shape.java
+++ src/fr/tm_nlm/tower_defence/control/data/geometric/Shape.java
@@ -1,5 +1,8 @@
 package fr.tm_nlm.tower_defence.control.data.geometric;
 
+import fr.tm_nlm.tower_defence.control.data.geometric.shape.Circle;
+import fr.tm_nlm.tower_defence.control.data.geometric.shape.Rect;
+
 public abstract class Shape {
 	private Vector position;
 	
@@ -8,6 +11,20 @@ public abstract class Shape {
 	}
 	
 	public abstract boolean collide(Shape shape);
+	
+	public Circle getCircle() {
+		if(this instanceof Circle) {
+			return (Circle) this;
+		}
+		throw new ClassCastException("Il ne s'agit pas d'un cerle.");
+	}
+	
+	public Rect getRect() {
+		if(this instanceof Rect) {
+			return (Rect) this;
+		}
+		throw new ClassCastException("Il ne s'agit pas d'un rectangle.");
+	}
 	
 	public void setPosition(Vector position) {
 		this.position = position;
