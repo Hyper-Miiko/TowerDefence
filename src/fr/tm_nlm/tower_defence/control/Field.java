ece583301fe3ac12a5ce02f4b117c6d8a552f5f1
Hyper-Miiko <geiishokunomiko@gmail.com>
2021-01-02 19:30:58 +0100

    gitignore towerdefence.java + plein de truc incomplet

--- src/fr/tm_nlm/tower_defence/control/Field.java
+++ src/fr/tm_nlm/tower_defence/control/Field.java
@@ -1,15 +1,53 @@
 package fr.tm_nlm.tower_defence.control;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 
-public class Field {
-	LinkedList<Entity> entities;
+import fr.tm_nlm.tower_defence.control.entity.FieldTile;
+import fr.tm_nlm.tower_defence.control.entity.Monster;
+import fr.tm_nlm.tower_defence.control.entity.Tower;
 
-	public Field() {
-		// TODO Auto-generated constructor stub
+public class Field {
+	private int lives;
+	private int temmies;
+	private LinkedList<Entity> entities;
+	private LinkedList<FieldTile> fieldTiles;
+	private LinkedList<Tower> towers;
+	private LinkedList<Monster> monsters;
+
+	public Field(int lives, int temmies) {
+		this.lives = lives;
+		this.temmies = temmies;
+		entities = new LinkedList<>();
+	}
+
+	public void add(Entity entity) {
+		entities.add(entity);
+		if(entity instanceof FieldTile) {
+			fieldTiles.add((FieldTile) entity);
+		} else if(entity instanceof Tower) {
+			towers.add((Tower) entity);
+		} else if(entity instanceof Monster) {
+			monsters.add((Monster) entity);
+		} else {
+			throw new InternalError("L'entité " + entity + " n'est pas reconnue");
+		}
 	}
 
 	public void remove(Entity entity) {
 		entities.remove(entity);
+		if(entity instanceof FieldTile) {
+			fieldTiles.remove(entity);
+		} else if(entity instanceof Tower) {
+			towers.remove(entity);
+		} else if(entity instanceof Monster) {
+			monsters.remove(entity);
+		} else {
+			throw new InternalError("L'entité " + entity + " n'est pas reconnue");
+		}
+	}
+	
+	public HashMap<Tower, Boolean> getAllTower() {
+		return Tower.getAddables(this);
 	}
 }
--- src/fr/tm_nlm/tower_defence/control/entity/Tower.java
+++ src/fr/tm_nlm/tower_defence/control/entity/Tower.java
@@ -1,14 +1,69 @@
 package fr.tm_nlm.tower_defence.control.entity;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+import fr.tm_nlm.tower_defence.Couple;
 import fr.tm_nlm.tower_defence.control.Entity;
 import fr.tm_nlm.tower_defence.control.Field;
 import fr.tm_nlm.tower_defence.control.data.geometric.Shape;
 
-public class Tower extends Entity {
-
-	public Tower(Field field, Shape shape) {
-		super(field, shape);
-		// TODO Auto-generated constructor stub
+public abstract class Tower extends Entity {
+	private static HashMap<Field, HashMap<Tower, Boolean>> addables = new HashMap<>();
+	
+	public static HashMap<Tower, Boolean> getAddables(Field field) {
+		return addables.get(field);
 	}
+	
+	private boolean obstacle;
+	private int cost;
+	private int evolveCost;
+	private double cooldown;
+	private double dammage;
+	private double health;
+	private double maxHealth;
+	private double range;
+	private String name;
+	private Tower evolution;
 
+	public Tower(Field field, Shape shape, String name) {
+		super(field, shape);
+		obstacle = false;
+		cost = 5;
+		evolveCost = -1;
+		cooldown = 1d;
+		dammage = 2d;
+		maxHealth = 20d;
+		health = maxHealth;
+		range = 100d;
+		this.name = name;
+		evolution = null;
+		
+		HashMap<Tower, Boolean> addables = getAddables(field);
+		if(addables == null) {
+			addables = new HashMap<>();
+			Tower.addables.put(field, addables);
+		}
+		addables.put(this, false);
+	}
+	
+	public Tower evolve() {
+		if(evolution == null) {
+			throw new IllegalStateException("L'entité n'a pas d'évolution disponible.");
+		}
+		HashMap<Tower, Boolean> addable = getAddables(field);
+		addable.put(this, false);
+		addable.put(evolution, true);
+		return evolution;
+	}
+	
+	public void setObstacle(boolean obstacle) {
+		this.obstacle = obstacle;
+	}
+	
+	public boolean isObstacle() {
+		return obstacle;
+	}
 }
