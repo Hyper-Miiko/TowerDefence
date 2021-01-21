ef033f55a8c75ba8010d9732927317504f04fcba
Hyper-Miiko <geiishokunomiko@gmail.com>
2021-01-21 13:11:40 +0100

    null image

--- src/fr/tm_nlm/tower_defence/control2/Displayable.java
+++ src/fr/tm_nlm/tower_defence/control2/Displayable.java
@@ -3,7 +3,7 @@ package fr.tm_nlm.tower_defence.control2;
 public interface Displayable extends Localisable {
 	public boolean isOnScreen();
 	public default boolean haveImage() {
-		return getImage() != null;
+		return !getImage().equals("data/img/null.png");
 	}
 	public String getImage();
 }
