517a013e3ca8b848388bd95771901417b72431e5
lodla <lodla@LAPTOP-QI3E77K6.home>
2021-01-24 12:56:52 +0100

    dogo

--- src/fr/tm_nlm/tower_defence/control/TowerDefence.java
+++ src/fr/tm_nlm/tower_defence/control/TowerDefence.java
@@ -1,45 +1,45 @@
-//package fr.tm_nlm.tower_defence.control;
-//
-//import mHUD.mGraphicEntity.*;
-//import mHUD.mObject.*;
-//import fr.tm_nlm.tower_defence.Constant;
-//import fr.tm_nlm.tower_defence.control.data.geometric.Vector;
-//import fr.tm_nlm.tower_defence.control.data.geometric.shape.Circle;
-//import fr.tm_nlm.tower_defence.control.entity.Monster;
-//import fr.tm_nlm.tower_defence.control.entity.Tower;
-//import fr.tm_nlm.tower_defence.control.entity.PresetTower;
-//import fr.tm_nlm.tower_defence.view.mHUDCompat.FieldToGraphic;
-//
-//public class TowerDefence {
-//
-//	public static void main(String[] args) throws InterruptedException{
-//		
-//		MWindow window = new MWindow(800, 600);
-//		
-//		FVerticalFrame mainFrame = new FVerticalFrame();
-//		mainFrame.setMinimumSize(800, 600);
-//		mainFrame.setBackgroundColor(100,0,100);
-//		window.setMainFrame(mainFrame);
-//		
-//		IGraphicView view = new IGraphicView(600, 600,1);
-//		view.setBackgroundColor(0, 0, 0);
-//		mainFrame.addObject(view);
-//		
-//		window.start();
-//		
-//		GRectEntity t = new GRectEntity(300,300,100,100);
-//		view.addGraphicEntityAt(0,t);
-//		
-//		boolean flag = false;
-//		while(true) {
-//			
-//			if(view.mousePressed() && !flag) {
-//				t.rotate(t.getRotation()+0.1);
-//				flag = true;
-//			}
-//			if(!view.mousePressed()) {
-//				flag = false;
-//			}
-//		}
-//	}
-//}
+package fr.tm_nlm.tower_defence.control;
+
+import mHUD.StdDraw;
+import mHUD.mObject.subWindow.GameWindow;
+import mHUD.mObject.subWindow.MenuWindow;
+
+public class TowerDefence {
+
+	public static void main(String[] args) throws InterruptedException {
+		boolean cheat = false;
+		if(args.length > 0) {
+			for(String a : args) {
+				if(a.equals("cheat")) {
+					cheat = true;
+					break;
+				}
+			}
+		}
+		MenuWindow menuWindow = new MenuWindow(980,714,cheat);
+		GameWindow gameWindow = new GameWindow(980,714);
+		
+		StdDraw.setCanvasSize(980,714);
+		StdDraw.enableDoubleBuffering();
+		
+		boolean inMenu = true;
+		while(true) {
+			if(inMenu) {
+				menuWindow.run();
+				int i = menuWindow.getSelection();
+				if(i != -1) {
+					gameWindow = new GameWindow(980,714);
+					gameWindow.setMap(i);
+					inMenu = false;
+				} 
+			}
+			else {
+				gameWindow.run();
+				if(gameWindow.isGameOver()) {
+					inMenu = true;
+					menuWindow = new MenuWindow(980,714,cheat);
+				}
+			}
+		}
+	}
+}
